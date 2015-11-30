0acbb126f1dcae55b79971fe4ef3aa7d5dde2971
rdrm2014 <ricardomendes@staff-0587.wireless.ua.pt>
2015-11-30 17:07:30 +0000

    Change 2

--- src/services/services.js
+++ src/services/services.js
@@ -16,59 +16,63 @@ exports.convertNodeRedtoXML = function (json) {
 
     var builder = require('xmlbuilder');
 
-    var nodered = json.data;
+    if(json.data) {
+        var nodered = json.data;
 
-    var graph = new DirectedGraph();
+        var graph = new DirectedGraph();
 
-    for (var i in nodered) {
-        var node = nodered[i];
-        if (node.func) {
-            graph.addNode(node.id, {name: node.name, type: node.type, func: node.func});
-        } else {
-            graph.addNode(node.id, {name: node.name, type: node.type});
-        }
-        if (node.wires) {
-            if (node.wires[0]) {
-                for (var a in node.wires[0]) {
+        for (var i in nodered) {
+            var node = nodered[i];
+            if (node.func) {
+                graph.addNode(node.id, {name: node.name, type: node.type, func: node.func});
+            } else {
+                graph.addNode(node.id, {name: node.name, type: node.type});
+            }
+            if (node.wires) {
+                if (node.wires[0]) {
+                    for (var a in node.wires[0]) {
 
-                    console.log(node.wires[0][a]);
-                    graph.addEdge(node.id + '->' + node.wires[0][a], node.id, node.wires[0][a]);
+                        console.log(node.wires[0][a]);
+                        graph.addEdge(node.id + '->' + node.wires[0][a], node.id, node.wires[0][a]);
+                    }
                 }
             }
         }
-    }
 
-    var sortedGraph = graph.tSort();
+        var sortedGraph = graph.tSort();
 
-    var node1 = graph.getNode(sortedGraph[0]);
-    var node2 = graph.getNode(sortedGraph[1]);
-    var node3 = graph.getNode(sortedGraph[2]);
+        var node1 = graph.getNode(sortedGraph[0]);
+        var node2 = graph.getNode(sortedGraph[1]);
+        var node3 = graph.getNode(sortedGraph[2]);
 
-    console.log(JSON.stringify(node3));
+        console.log(JSON.stringify(node3));
 
-    //var name = "Hello World";
-    if (node1.func && node2.func && node3.func) {
+        //var name = "Hello World";
+        if (node1.func && node2.func && node3.func) {
 
-        var expr1 = node1.func;//"status == Message.HELLO";
-        var expr2 = node3.func;//"myMessage : message";
-        var rhs = node2.func;//'System.out.println( myMessage ); m.setMessage( "Goodbye cruel world" ); m.setStatus( Message.GOODBYE ); update( m );';
+            var expr1 = node1.func;//"status == Message.HELLO";
+            var expr2 = node3.func;//"myMessage : message";
+            var rhs = node2.func;//'System.out.println( myMessage ); m.setMessage( "Goodbye cruel world" ); m.setStatus( Message.GOODBYE ); update( m );';
 
-        var xml = builder.create('package').att('name', "com.sample").att('xmlns', "http://drools.org/drools-5.2").att('xmlns:xs', "http://www.w3.org/2001/XMLSchema-instance").att('xs:schemaLocation', "http://drools.org/drools-5.2 drools.org/drools-5.2.xsd");
+            var xml = builder.create('package').att('name', "com.sample").att('xmlns', "http://drools.org/drools-5.2").att('xmlns:xs', "http://www.w3.org/2001/XMLSchema-instance").att('xs:schemaLocation', "http://drools.org/drools-5.2 drools.org/drools-5.2.xsd");
 
-        xml.ele('import', {'name': "com.sample.DroolsTest.Message"});
+            xml.ele('import', {'name': "com.sample.DroolsTest.Message"});
 
-        var ruleElem = xml.ele('rule', {'name': ruleName});
-        var lhsElem = ruleElem.ele('lhs');
-        var patternElem = lhsElem.ele('pattern', {'object-type': "Message"});
+            var ruleElem = xml.ele('rule', {'name': ruleName});
+            var lhsElem = ruleElem.ele('lhs');
+            var patternElem = lhsElem.ele('pattern', {'object-type': "Message"});
 
-        patternElem.ele('expr', {}, expr1);
-        patternElem.ele('expr', {}, expr2);
+            patternElem.ele('expr', {}, expr1);
+            patternElem.ele('expr', {}, expr2);
 
-        ruleElem.ele('rhs', {}, rhs);
+            ruleElem.ele('rhs', {}, rhs);
 
-        var xmlString = xml.end({pretty: true});
+            var xmlString = xml.end({pretty: true});
 
-        return {name: ruleName, rule: xmlString};
+            return {name: ruleName, rule: xmlString};
+        } else {
+            return {error: 'ERROR'};
+        }
     } else {
         return {error: 'ERROR'};
     }
